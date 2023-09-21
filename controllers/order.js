ad909fc360f85e3468efb8ff308490cc2ea7ad7e
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-09-21 20:52:48 -0300

    Finished the order logic. Everything is fine now. :)

--- controllers/order.js
+++ controllers/order.js
@@ -1,20 +1,59 @@
 const Cattle = require('../models/cattle');
 const Customer = require('../models/customer');
+const Order = require('../models/order');
 
 
 exports.findOrder = (req, res, next) => {
-    res.json({
-        hehe: ':)',
-    });
+    
+    const cattleId = req.query.cattleId;
+    const customerId = req.query.customerId;
+
+    const showCustomer = req.query.showCustomer;
+
+    const fildsToFilter = {};
+    const includes = [];
+
+    if (cattleId) {
+        fildsToFilter.cattleId = cattleId;
+    }
+
+    if (customerId) {
+        fildsToFilter.customerId = customerId;
+    }
+
+    if (showCustomer) {
+        includes.push({
+            model: Customer,
+            required: true
+        })
+    }
+
+    Order.findAll({
+        where: fildsToFilter,
+        include: includes
+    })
+    .then(data => {
+        res.json(data);
+    })  
+    .catch(err => console.log(err));
 }
 
 exports.newOrder = (req, res, next) => {
     const customerId = req.body.customerId;
     const cattleId = req.body.cattleId;
-    const discountPercentage = req.body.discountPercentage;
-    const totalDiscountValue = req.body.totalDiscountValue;
-    const ValueKG = req.body.ValueKG;
+    const discountPercentage = parseFloat(req.body.discountPercentage);
+    const totalDiscountValue = parseFloat(req.body.totalDiscountValue);
+    const ValueKG = parseFloat(req.body.ValueKG);
 
-    //<-----------
-
+    Order.create({
+        customerId: customerId,
+        cattleId: cattleId,
+        discountPercentage: discountPercentage,
+        totalDiscountValue: totalDiscountValue,
+        ValueKG: ValueKG,
+    })
+    .then(success => {
+        res.json(success);
+    })
+    .catch(err => console.log(err));
 }
